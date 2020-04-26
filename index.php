6d5387440fae640b41d475cb9479189d9dc9394a
Iamsamuelemo <emoavwoduasamuel@gmail.com>
2020-04-26 09:49:55 +0100

    Added Index.php

--- index.php
+++ index.php
@@ -1,8 +1,54 @@
-echo "# Samuelproject_1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Iamsamuelemo/Samuelproject_1.git
-git push -u origin master
+<?php 
+
+//$var = "my name is solomon keen";
+//$var5 =5;
+//$var51 =5;
+
+//echo $var;
+//echo $var5 + $var51;
+
+
+
+//echo "<br>";
+
+//if ($var51==6) {
+   // echo $var;
+//}elseif($var51>10) {
+  //  echo "This is the second ans";
+//}else{
+ //       echo "This is your final answer";
+//}
+
+
+if (isset($_POST["name"])) {
+    $var=$_POST["name"];
+}
+else{
+    $var= ["Tony", "John", "Solomon", "Daniel"];
+}
+
+
+$varform="<html>
+                <head></head>
+                <body>
+                    <h1> welcome to my php code</h1>
+                    <form action='http://localhost/lessons/index.php' method='POST'>
+                        <input type='text'  name='name'/>
+                        <input type='submit' name='submit'/>
+                    </form>
+                </body>
+            </html>";
+
+            foreach ($var as $prm) {
+                echo $prm;
+                echo "<br>";
+            }
+
+            array_push($var, "Smart");
+            array_pop($var);
+            $var[1] = "Omada";
+            print_r($var)
+            
+?>
                 
                 
