e88b589dcabf189279991129907c9f40a9bbb53c
Samuel Emo <64349812+Iamsamuelemo@users.noreply.github.com>
2020-04-26 09:19:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+echo "# Samuelproject_1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Iamsamuelemo/Samuelproject_1.git
+git push -u origin master
+                
+                
